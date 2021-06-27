97628eec098a7429288f040dac16e329a9eb4c1a
Nayron Ferreira <nayronferreira30@gmail.com>
2021-06-27 14:33:21 -0300

    Criando relação Muito para Muito no paradgma Banco Relacional

--- src/main/java/com/projetoCurso/configaration/TesteConfig.java
+++ src/main/java/com/projetoCurso/configaration/TesteConfig.java
@@ -44,6 +44,20 @@ public class TesteConfig implements CommandLineRunner{
 		Produto prod4 = new Produto(null, "A arte de Argumentar", "Aprenda a argumentar tudo e nunca perder um debate!", 5.000, "");
 		Produto prod5 = new Produto(null, "Notebook Gamer", "Processador i7 10g, 2T GB, Placa Video 3 GB", 6.000, "");
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
+		prod1.getCategoria().add(cat3);
+		prod1.getCategoria().add(cat1);
+		prod2.getCategoria().add(cat3);
+		prod2.getCategoria().add(cat1);
+		prod3.getCategoria().add(cat2);
+		prod4.getCategoria().add(cat2);
+		prod5.getCategoria().add(cat3);
+		prod5.getCategoria().add(cat1);
+		
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
 		Usuario u1 = new Usuario(null, "Anna May", "annamay@gmail.com", "988888888", "123456");
 		Usuario u2 = new Usuario(null, "Nayron Ferreira", "nayronferreira@gmail.com", "977777777", "123456");
 		
@@ -51,8 +65,7 @@ public class TesteConfig implements CommandLineRunner{
 		Pedido p2 = new Pedido(null, Instant.parse("2021-10-18T11:01:02Z"),PedidoStatus.AGUARDANDO_PAGAMENTO,u1);
 		Pedido p3 = new Pedido(null, Instant.parse("2021-10-18T12:01:02Z"),PedidoStatus.CANCELADO, u1);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3,prod4,prod5));
+		
 		urepository.saveAll(Arrays.asList(u1,u2));
 		pedidoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
