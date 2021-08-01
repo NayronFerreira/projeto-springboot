52b4b071652c7b3ee3360265eaab735adc1da338
Nayron Ferreira <nayronferreira30@gmail.com>
2021-08-01 13:12:49 -0300

    Criando Exceção personalizada para a camada de serviço e resource do Usuario Para o método DELETE

--- src/main/java/com/projetoCurso/services/UsuarioService.java
+++ src/main/java/com/projetoCurso/services/UsuarioService.java
@@ -4,37 +4,51 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetoCurso.entidades.Usuario;
 import com.projetoCurso.repositories.UsuarioRepositoy;
+import com.projetoCurso.services.exceptions.DataBaseException;
 import com.projetoCurso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepositoy userRepository;
 
-	public List <Usuario> findAlll () {
+	public List<Usuario> findAlll() {
 		return userRepository.findAll();
 	}
-	public Usuario findById (Long id) {
-		 Optional<Usuario> obj = userRepository.findById(id);
-		 return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
+	public Usuario findById(Long id) {
+		Optional<Usuario> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	public Usuario inserir (Usuario usuario) {
+
+	public Usuario inserir(Usuario usuario) {
 		return userRepository.save(usuario);
 	}
-	public void deletar (Long id) {
-		userRepository.deleteById(id); 		
+
+	public void deletar(Long id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException di) {
+			throw new DataBaseException(di.getMessage());
+		}
 	}
-	public Usuario atualizar (Long id, Usuario obj) {
+
+	public Usuario atualizar(Long id, Usuario obj) {
 		Usuario entity = userRepository.getOne(id);
 		atualizarDados(entity, obj);
 		return userRepository.save(entity);
 	}
-	public void atualizarDados (Usuario entity, Usuario obj ) {
+
+	public void atualizarDados(Usuario entity, Usuario obj) {
 		entity.setNome(obj.getNome());
 		entity.setEmail(obj.getEmail());
 		entity.setTelefone(obj.getTelefone());
